4b8468b16ea85d676ead287751c0f560c457d1a2
Felix Zhou <felix990302@yahoo.ca>
2018-01-24 20:12:24 -0500

    write parser and interpreter

---
+++ oct2tex.py
@@ -0,0 +1,80 @@
+'''
+usage:
+cat about.txt | python soinput.py
+'''
+
+
+import sys
+
+
+def read_in():
+    lines = sys.stdin.read()
+
+    return lines
+
+
+def tokenize(input):
+    tokens = []
+    temp=""
+    for i in input:
+        if(i=="\n" or i==" " or i==","):
+            if(temp!=""):
+                tokens.append(numberize(temp))
+                temp = ""
+
+        elif(i==";"):
+            tokens.append(numberize(temp))
+            tokens.append(";")
+            temp=""
+
+        else:
+            temp += i
+
+    return tokens
+
+def numberize(s):
+    try:
+        ans = int(s)
+        return ans
+    except Exception:
+        try:
+            ans = float(s)
+            return ans
+        except Exception:
+            return s
+
+
+def interp(tokens):
+
+    prev = 0
+    for ind in range(len(tokens)):
+        t = tokens[ind]
+
+        if(t=="["):
+            prev = t
+            tokens[ind] = "\\begin{bmatrix}\n"
+        elif(t=="]"):
+            prev = t
+            tokens[ind] = "\n\\end{bmatrix}\n"
+        elif(t==";"):
+            prev = t
+            tokens[ind] = "\\\\\n"
+        else:
+            prev = t
+
+        if(isinstance(prev, int) or isinstance(prev, float)):
+            tokens[ind] = " & " + str(t)
+
+    return tokens
+
+
+def main():
+    out = interp(tokenize(read_in()))
+
+    for s in out:
+        print(s, end="", flush=True)
+
+
+
+if __name__ == '__main__':
+    main()
